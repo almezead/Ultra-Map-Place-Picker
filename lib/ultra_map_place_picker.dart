4d4cb609aff64ca8d39e5e33c4ac735a20903510
almezead <ali@bravo-global.co>
2024-10-27 03:26:53 +0300

    test

--- lib/ultra_map_place_picker.dart
+++ lib/ultra_map_place_picker.dart
@@ -3,6 +3,8 @@ export 'src/widgets/ultra_map_place_picker.dart';
 export 'src/widgets/map_search_bar.dart';
 export 'src/widgets/auto_complete_search.dart';
 
+export 'src/controllers/auto_complete_search_controller.dart';
+
 export 'src/models/ultra_polygon_model.dart';
 export 'src/models/ultra_polyline_model.dart';
 export 'src/models/location_model.dart';
